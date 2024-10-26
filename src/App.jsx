2e5da0554c68f7a65e71a04e0ebfc1c38aa9924e
isikmert <116586106+isikmert1@users.noreply.github.com>
2024-10-26 03:17:57 +0300

    Implemented Basic Structure And Functionality For The Landing Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,91 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
+import Navbar from './components/Navbar';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [selectedIndex, setSelectedIndex] = useState(0);
+
+  const handleClick = (index) => {
+    setSelectedIndex(index);
+  };
+
+  const renderComponent = () => {
+    switch (selectedIndex) {
+      case 0:
+        return (
+          <section className="calendar-container">
+            <p>Lütfen rezervasyon tarihi seçiniz.</p>
+            <Calendar />  
+          </section>
+        );
+      case 1:
+        return (
+          <section className="time-container">
+            <p>Lütfen rezervasyon saati seçiniz.</p>
+            {/* <Time /> //TODO: Create Time component */}  
+          </section>
+        );
+      case 2:
+        return (
+          <section className="guest-container">
+            <p>Lütfen misafir sayısını seçiniz.</p>
+            {/* <Guest /> //TODO: Create Guest component */}
+          </section>
+        );
+      case 3:
+        return (
+          <section className="request-container">
+            <p>Etiket seçebilir ve notunuz varsa rezervasyonunuzu özelleştirebilirsiniz.</p>
+            {/* <Request /> //TODO: Create Request component */}
+          </section>
+        );
+      case 4:
+        return (
+          <section className="info-container">
+            <p>Lütfen rezervasyonunuzu tamamlayın.</p>
+            {/* <Info /> //TODO: Create Info component */}
+          </section>
+        );
+      default:
+        return null;
+    }
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="app">
+      <header>
+        <Navbar />
+      </header>
+      <main>
+        <section className="intro-container">
+          <h1 className="intro-title">Hoş Geldiniz!</h1>
+          <p className="intro-para">
+            Aşağıdaki bilgileri doldurarak, rezervasyon talebinizi gönderin. 
+            Rezervasyon talep ettiğiniz şube sizi müsaitlik durumuna göre 
+            onaylayabilir, sıraya alabilir ya da sizi bilgilendirerek iptal edebilir.
+          </p>
+        </section>
+        <section className="selection-container">
+          {['Tarih', 'Saat', 'Misafir', 'İstek', 'Bilgi'].map((item, index) => (
+            <button
+              key={index}
+              className={`menu-item ${selectedIndex === index ? 'clicked' : ''}`}
+              onClick={() => handleClick(index)} 
+            >
+              <img src="https://placehold.co/50x50" alt="icon" className="item-img" />
+              <p className="item-para">{item}</p>
+            </button>
+          ))}
+        </section>
+        {renderComponent()}
+      </main>
+      <footer>
+        {/* <Footer /> //TODO: Do Footer */}
+      </footer>
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import './Navbar.css'
+
+function Navbar () {
+
+    return (
+        <nav className='navbar'>
+            <div className='rest-logo'>Restaurant_Logo</div>
+        </nav>
+    )
+}
+
+export default Navbar;
